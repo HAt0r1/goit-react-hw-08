76e71b96a26566f198cdf7050caa2558b974758f
Hatori <ahgangadm@gmail.com>
2024-05-16 20:30:55 +0300

    Add filters reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/slice";
+import filterSlice from "./filters/slice";
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     // contacts:,
-    // filters:,
+    filters: filterSlice,
   },
 });
