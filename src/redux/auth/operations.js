5d0b3844aba89fdd92ab28891fee504177073f83
Hatori <ahgangadm@gmail.com>
2024-05-17 23:07:34 +0300

    Create operations for auth

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,2 +1,50 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+
+axios.defaults.baseURL = "https://connections-api.herokuapp.com";
+
+const setAuthHeader = (token) => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const clearAuthHeader = () => {
+  axios.defaults.headers.common.Authorization = "";
+};
+
+export const signUser = createAsyncThunk(
+  "auth/signup",
+  async (newUser, thunkAPI) => {
+    try {
+      const responce = await axios.post("/users/signup", newUser);
+      setAuthHeader(responce.data.token);
+      return responce.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  "auth/login",
+  async (user, thunkAPI) => {
+    try {
+      const responce = await axios.post("/users/login", user);
+      setAuthHeader(responce.data.token);
+      return responce.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logOutUser = createAsyncThunk(
+  "auth/logout",
+  async (_, thunkAPI) => {
+    try {
+      const responce = await axios.post("/users/logout");
+      clearAuthHeader();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
