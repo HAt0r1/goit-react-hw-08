445f64859f884970e295a7b409bb16eacfd7cfdf
Hatori <ahgangadm@gmail.com>
2024-05-17 23:05:38 +0300

    Add contacts reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/slice";
 import filterSlice from "./filters/slice";
+import contactsSlice from "./contacts/slice";
 
 export default configureStore({
   reducer: {
     auth: authReducer,
-    // contacts:,
+    contacts: contactsSlice,
     filters: filterSlice,
   },
 });
