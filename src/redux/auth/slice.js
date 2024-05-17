4cc97d19c5c7a3c7448a0ac49e1c78b2c5b37184
Hatori <ahgangadm@gmail.com>
2024-05-17 23:08:19 +0300

    Add basic cases for auth slice

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,4 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { logOutUser, signUser } from "./operations";
+import { loginUser } from "./operations";
 
 const authSlice = createSlice({
   name: "auth",
@@ -10,6 +12,24 @@ const authSlice = createSlice({
     token: null,
     isLoggedIn: false,
     isRefreshing: false,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(signUser.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.isLoggedIn = true;
+        state.token = action.payload.token;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.isLoggedIn = true;
+        state.token = action.payload.token;
+      })
+      .addCase(logOutUser.fulfilled, (state) => {
+        state.user = { name: null, email: null };
+        state.token = null;
+        state.isLoggedIn = false;
+      });
   },
 });
 
