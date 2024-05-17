008df2dd6a68d532cf0dbd656b1d2d400366e694
Hatori <ahgangadm@gmail.com>
2024-05-17 23:06:29 +0300

    Add markup for homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,15 @@
-const HomePage = () => {};
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Contacts</h1>
+      <p>
+        This is designed to give you the opportunity to create and save your
+        contacts. If you want to try how is it working, first of all you need to
+        registration account. After that you can do anathing you want with your
+        contacts.
+      </p>
+    </div>
+  );
+};
 
 export default HomePage;
