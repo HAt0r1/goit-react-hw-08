5b1fcb9e7eecf81ac28a6547f5051f02b749cd17
Hatori <ahgangadm@gmail.com>
2024-05-18 18:39:55 +0300

    Add cases for contacts reducer

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchContacts, addContact, deleteContact } from "./operations";
 const setLoading = (state) => {
   state.loading = true;
 };
@@ -13,6 +14,31 @@ const contactSlice = createSlice({
     items: [],
     loading: false,
     error: null,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchContacts.pending, setLoading)
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = action.payload;
+      })
+      .addCase(fetchContacts.rejected, setError)
+      .addCase(addContact.pending, setLoading)
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.items.push(action.payload);
+      })
+      .addCase(addContact.rejected, setError)
+      .addCase(deleteContact.pending, setLoading)
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.items = state.items.filter(
+          (contact) => contact.id !== action.payload.id
+        );
+      })
+      .addCase(deleteContact.rejected, setError);
   },
 });
 
