9da9db222eecc20884fac8e08724cc779f1b997b
Hatori <ahgangadm@gmail.com>
2024-05-18 18:40:13 +0300

    Add selectors

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,1 +1,15 @@
-export const selectContacts = (state) => state;
+import { createSelector } from "@reduxjs/toolkit";
+
+export const selectContacts = (state) => state.contacts.items;
+export const selectError = (state) => state.contacts.error;
+export const selectLoading = (state) => state.contacts.loading;
+export const selectFilter = (state) => state.filter.name;
+
+export const filterContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+);
--- src/redux/filters/selectors.js
+++ src/redux/filters/selectors.js
@@ -1,1 +1,1 @@
-export const filter = (state) => state.filters.name;
+export const selectFilter = (state) => state.filters.name;
